69c540116b476e5bc1adf08f17bcb8cdfadd3c69
BulyginK <buligin.k@gmail.com>
2022-03-30 11:25:04 +0300

    Внес правки

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ let service1 = prompt('Какой дополнительный тип услуг
 let servicePrice1 = prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = prompt('Сколько это будет стоить?');
-let rollback = 20;
-let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice * (1 - rollback/100));
 let allServicePrices;
+let fullPrice;
+let rollback = 20;
+let servicePercentPrice;
+
+
+const getTitle = function(title) {
+    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
+}
+
+const getAllServicePrices = function(serPr1, serPr2) {
+    return +serPr1 + +serPr2;
+}
+
+function getFullPrice(scrPr, allSerPr) {
+    return +scrPr + allSerPr;
+}
+
+const getServicePercentPrices = function(fPrice, rBack) {
+    return Math.ceil(fPrice * (1 - rBack/100));
+}
 
 const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
@@ -29,30 +46,14 @@ const getRollbackMessage = function(price) {
     }
 }
 
-const getAllServicePrices = function(serPr1, serPr2) {
-    allServicePrices = serPr1 + serPr2;
-}
-
-function getFullPrice(scrPr, allSerPr) {
-    fullPrice = scrPr + allSerPr;
-}
-
-const getTitle = function(title) {
-    return title.trim().charAt(0).toUpperCase() + title.trim().slice(1).toLowerCase();
-}
-
-const getServicePercentPrices = function(fPrice, rBack) {
-    servicePercentPrice = Math.ceil(fPrice * (1 - rBack/100));
-}
+title = getTitle(title);
+allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
 showTypeOf(title);
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
-
-getAllServicePrices(+servicePrice1, +servicePrice2);
-getFullPrice(+screenPrice, allServicePrices);
-getTitle(title);
-getServicePercentPrices(fullPrice, rollback);
 
 console.log(screens.toLowerCase().split(", "));
 console.log(getRollbackMessage(fullPrice));
