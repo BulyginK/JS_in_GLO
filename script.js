a96cdc4d7f56f5ab538557f81903b078092b6189
BulyginK <bulygin.k@gmail.com>
2022-03-31 23:00:12 +0300

    Правки

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ let fullPrice;
 let servicePercentPrice;
 
 // Проверка на число
-const isNamber = function (num) {
+const isNamber = function(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 
-const asking = function () {
+const asking = function() {
     title = prompt("Как называется ваш проект?", "Калькулятор верстки");
     screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
     do {
@@ -26,11 +26,11 @@ const asking = function () {
 }
 
 // Переводим текст на вариант "первый символ с большой буквы, остальные с маленькой (+убираем пробелы вначале)"
-const getTitle = function (str) {
+const getTitle = function(str) {
     return str.trim().charAt(0).toUpperCase() + str.trim().slice(1).toLowerCase();
 }
 
-const getAllServicePrices = function () {
+const getAllServicePrices = function() {
     let sum = 0;
     for (let i = 0; i < 2; i++) {
         if (i === 0) {
@@ -38,27 +38,28 @@ const getAllServicePrices = function () {
         } else if (i === 1) {
             service2 = prompt("Какой дополнительный тип услуги нужен?", "Адаптив");
         }
-        sum += +prompt('Сколько это будет стоить?');
-        while (!isNamber(sum)) {
-            sum = +prompt('Сколько это будет стоить?');
+        let servicePrice = +prompt('Сколько это будет стоить?');
+        while (!isNamber(servicePrice)) {
+            servicePrice = +prompt('Сколько это будет стоить?');
         }
+        sum += servicePrice;
     }
     return sum;
 }
 
-function getFullPrice () {
+function getFullPrice() {
     return screenPrice + allServicePrices;
 }
 
-const getServicePercentPrices = function () {
+const getServicePercentPrices = function() {
     return Math.ceil(fullPrice * (1 - rollback/100));
 }
 
-const showTypeOf = function (variable) {
+const showTypeOf = function(variable) {
     console.log(variable, typeof variable);
 }
 
-const getRollbackMessage = function (price) {
+const getRollbackMessage = function(price) {
     if (price >= 30000) {
         return "Даем скидку в 10%";
     } else if (price >= 15000) {
